528ee3fc2628a89f1ed45fa9875e594c13d5723a
uporoje <34911292+uporoje@users.noreply.github.com>
2019-12-20 14:41:41 +0300

    Add project files.

---
+++ ConsoleApp-BlockChain/Blockchain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_BlockChain
+{
+   public class Blockchain
+    {
+        public IList<Block> chain { get; set; }
+        public int Difficulty { set; get; } = 1;
+        public Blockchain()
+        {
+            InitializeChain();
+            AddGenesisBlock();
+        }
+        //Initialize
+        private void InitializeChain()
+        {
+            chain = new List<Block>();
+        }
+        private void AddGenesisBlock()
+        {
+            chain.Add(CreateGenesisBlock());
+        }
+        public Block CreateGenesisBlock()
+        {
+            return new Block(DateTime.Now, null, "{}");
+        }
+        //end Initialize
+
+        public void AddBlock(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.Index = latestBlock.Index + 1;
+            block.PreviousHash = latestBlock.Hash;
+            block.Mine(this.Difficulty);
+            chain.Add(block);
+        }
+        public Block GetLatestBlock()
+        {
+            return chain[chain.Count - 1];
+        }
+     
+     
+        public bool IsValid()
+        {
+            for (int i = 1; i < chain.Count; i++)
+            {
+                Block currentBlock = chain[i];
+                Block previousBlock = chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp-BlockChain/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace ConsoleApp_BlockChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Blockchain TechCoin = new Blockchain();
+            var startTime1 = DateTime.Now;
+            TechCoin.AddBlock(new Block(DateTime.Now, null, "{sender:ahmet,receiver:orhan,amount:10}"));
+            Console.WriteLine($"Duration1: {DateTime.Now - startTime1}");
+            var startTime2 = DateTime.Now;
+            TechCoin.AddBlock(new Block(DateTime.Now, null, "{sender:orhan,receiver:ahmet,amount:5}"));
+            Console.WriteLine($"Duration2: {DateTime.Now - startTime2}");
+            var startTime3 = DateTime.Now;
+            TechCoin.AddBlock(new Block(DateTime.Now, null, "{sender:orhan,receiver:ahmet,amount:5}"));
+            Console.WriteLine($"Duration3: {DateTime.Now - startTime3}");
+
+            //  var endTime = DateTime.Now;
+
+
+
+
+            Console.WriteLine(JsonConvert.SerializeObject(TechCoin, Formatting.Indented));
+            Console.WriteLine(TechCoin.IsValid());
+            TechCoin.chain[1].Data = "{sender:ahmet,receiver:orhan,amount:1000}";
+            Console.WriteLine(TechCoin.IsValid());
+            
+            Console.ReadKey();
+        }
+    }
+}
